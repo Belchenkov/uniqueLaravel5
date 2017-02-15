e5ce10e1a6fc17457a54884bca5572d37a97b87f
Belchenkov <u608110@gmail.com>
2017-02-15 19:36:31 +0300

    Create models

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -6,10 +6,24 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 
+//Models
+use App\Page;
+use App\Portfolio;
+use App\Service;
+use App\People;
+
 class IndexController extends Controller
 {
     public function execute(Request $request)
     {
+        $pages = Page::all();
+        $portfolio = Portfolio::get(['name', 'filter', 'images']);
+        $services = Service::where('id', '>', 3)->get();
+        $people = People::take(3)->get();
+        
+        dd($pages);
+        
         return view('layouts.site');
     }
 }
+  
